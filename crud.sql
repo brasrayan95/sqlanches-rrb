e7acd6029e72705cd488a85164d74cd31b15cd4e
Rayan Brasil <brasrayan@gmail.com>
2022-10-09 22:02:32 -0400

    done

--- crud.sql
+++ crud.sql
@@ -3,32 +3,77 @@
 
 -- Criação
 
--- 1)
+-- 1)INSERT INTO
+	clientes(nome, lealdade)
+VALUES 
+	('Georgia', 0);
 
 
 
--- 2)
+-- 2)INSERT INTO
+	pedidos(status, cliente_id)
+VALUES
+	('Recebido', 6)
 
 
 
--- 3)
+-- 3)INSERT INTO 
+	produtos_pedidos(pedido_id, produto_id)
+VALUES 
+	(6, 1),
+	(6, 2),
+	(6, 6),
+	(6, 8),
+	(6, 8);
 
 
 
 -- Leitura
 
 -- 1)
+SELECT
+	cl.id, cl.nome, cl.lealdade, pe.id, pe.status, pe.cliente_id, po.id, po.nome, po.tipo, po.preco, po.pts_de_lealdade 
+FROM
+	clientes cl
+JOIN
+	pedidos pe ON pe.id = cl.id
+JOIN 
+	produtos_pedidos pp ON pp.pedido_id = pe.id
+JOIN 
+	produtos po ON po.id = pp.produto_id
+WHERE
+	cl.nome = 'Georgia';
 
 
 
 -- Atualização
 
 -- 1)
-
+UPDATE 
+	clientes
+SET
+	lealdade = (SELECT
+	sum(po.pts_de_lealdade)
+FROM
+	clientes cl
+JOIN
+	pedidos pe ON pe.id = cl.id
+JOIN 
+	produtos_pedidos pp ON pp.pedido_id = pe.id
+JOIN 
+	produtos po ON po.id = pp.produto_id
+WHERE
+	cl.nome = 'Georgia')
+WHERE nome = 'Georgia';
 
 
 -- Deleção
 
 -- 1)
-
+DELETE
+	FROM clientes
+WHERE 
+	nome = 'Marcelo'
+RETURNING *;
+	
 
--- selects.sql
+++ selects.sql
@@ -2,11 +2,65 @@
 -- Seleções de dados
 
 -- 1)
-
+SELECT 
+	pe.id,
+	pe.status,
+	pe.cliente_id,
+	po.id,
+	po.nome,
+	po.tipo,
+	round(CAST(po.preco AS NUMERIC), 2) preço,
+	po.pts_de_lealdade
+FROM
+	pedidos pe
+JOIN
+	produtos_pedidos pp ON pe.id = pp.pedido_id
+JOIN
+	produtos po ON po.id = pp.produto_id;
 -- 2)
-
+SELECT
+	pe.id
+FROM
+	pedidos pe
+JOIN
+	produtos_pedidos pp ON pe.id = pp.pedido_id
+JOIN
+	produtos po ON po.id = pp.produto_id
+WHERE 
+	nome LIKE '%Fritas%';
 -- 3)
-
+SELECT
+	clientes.nome AS gostam_de_fritas,
+FROM
+	pedidos pe
+JOIN
+	produtos_pedidos pp ON pe.id = pp.pedido_id
+JOIN
+	produtos po ON po.id = pp.produto_id
+JOIN
+	clientes ON clientes.id = pe.cliente_id 
+WHERE 
+	po.nome LIKE '%Fritas%';
 -- 4)
+SELECT
+	SUM(preco)
+FROM
+	pedidos pe
+JOIN
+	produtos_pedidos pp ON pe.id = pp.pedido_id
+JOIN
+	produtos po ON po.id = pp.produto_id
+JOIN
+	clientes ON clientes.id = pe.cliente_id
+WHERE
+	clientes.nome = 'Laura';
+-- 5)--n consegui terminar
 
--- 5)
+SELECT
+	po.nome,
+	count()
+FROM
+	pedidos pe
+JOIN
+	produtos po ON pe.id = po.id;
+
